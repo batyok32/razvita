05a4f1f9723f6c78b6b9e7227e80168233f0d6b8
batyok32 <betoglan041@gmail.com>
2025-04-09 06:38:41 -0700

    Added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://yourdomain.com", // Replace with your actual domain
+    generateRobotsTxt: true, // (Optional) Generates robots.txt file
+    changefreq: "daily",
+    priority: 0.7,
+    sitemapSize: 5000,
+    exclude: ["/admin"], // Add any paths you want to exclude
+};
